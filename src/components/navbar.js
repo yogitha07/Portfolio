8df9bdf2c0a0cf84e88a3941a05acaad27b3ba76
Yogitha <59954603+yogitha07@users.noreply.github.com>
2023-11-08 23:04:21 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -42,21 +42,26 @@ export default function NavBar() {
       <nav
         className={!toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}
       >
-        <ul className="flex flex-col">
+        <ul className="flex">
           <li>
-            <a href="/#about">About</a>
+            <a href="#about">About</a>
           </li>
           <li>
-            <a href="/#projects">Projects</a>
+            <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="/#blogs">Blog</a>
+            <a href="#experience">Work Experience</a>
           </li>
           <li>
-            <a href="/#contact">Contact</a>
+            <a href="#blogs">Blog</a>
           </li>
           <li>
-            <a href="/#resume">Resume</a>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="https://drive.google.com/file/d/1cAehCNrlU1ClplgrhIyMbQBpVjcrnYTD/view?usp=sharing" target="_blank" without rel="noreferrer">
+              Cover Letter
+            </a>
           </li>
         </ul>
       </nav>
