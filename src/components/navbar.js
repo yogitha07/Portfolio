529427a2d65e27a632f32106a9db4299befe6f4e
yogitha07 <vijaya.077yogitha.99@gmail.com>
2023-11-09 21:44:01 +0530

    Another Commit for ph-pages branch

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ export default function NavBar() {
         Yogitha Muthappa
       </a>
 
-      {/* Desktop Nav */}
+    {/* Desktop Nav */}
       <nav className="hidden md:block">
         <ul className="flex">
           <li>
@@ -39,32 +39,31 @@ export default function NavBar() {
       </nav>
 
       {/* Mobile Nav */}
-      <nav
-        className={!toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}
-      >
-        <ul className="flex">
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#experience">Work Experience</a>
-          </li>
-          <li>
-            <a href="#blogs">Blog</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-          <li>
-            <a href="https://drive.google.com/file/d/1cAehCNrlU1ClplgrhIyMbQBpVjcrnYTD/view?usp=sharing" target="_blank" without rel="noreferrer">
-              Cover Letter
-            </a>
-          </li>
-        </ul>
-      </nav>
+<nav className={!toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}>
+  <ul className="flex flex-col py-4">
+    <li className="py-4 ">
+      <a href="#about" onClick={() => setToggle(false)}>About</a>
+    </li>
+    <li className="py-4 ">
+      <a href="#projects" onClick={() => setToggle(false)}>Projects</a>
+    </li>
+    <li className="py-4 ">
+      <a href="#experience" onClick={() => setToggle(false)}>Work Experience</a>
+    </li>
+    <li className="py-4 ">
+      <a href="#blogs" onClick={() => setToggle(false)}>Blog</a>
+    </li>
+    <li className="py-4 ">
+      <a href="#contact" onClick={() => setToggle(false)}>Contact</a>
+    </li>
+    <li className="py-4 ">
+      <a href="https://drive.google.com/file/d/1cAehCNrlU1ClplgrhIyMbQBpVjcrnYTD/view?usp=sharing" target="_blank" without rel="noreferrer" onClick={() => setToggle(false)}>
+        Cover Letter
+      </a>
+    </li>
+  </ul>
+</nav>
+
 
       {/* Toggle button */}
       <button onClick={handleToggle} className="block md:hidden">
